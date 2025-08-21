06eb41aa4f62146698bf35326cfccf790d8b50f6
GustavoRRSilva <gstvrrds@gmail.com>
2025-08-21 14:32:19 -0300

    feat:adding form validation with reactiveform

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -7,9 +7,26 @@ import { FormBuilder,FormGroup,Validators } from '@angular/forms';
   styleUrls: ['./reservation-form.component.css']
 })
 export class ReservationFormComponent {
+
   reservationForm:FormGroup = new FormGroup({})
 
-  onSubmit(){
+  constructor(private formBuilder:FormBuilder) {
+    this.formBuilder = formBuilder;
+  }
 
+  ngOnInit(){
+    this.reservationForm = this.formBuilder.group({
+      checkInDate:['',Validators.required],
+      checkOutDate:['',Validators.required],
+      guestName:['',Validators.required],
+      guestEmail:['',[Validators.required,Validators.email]],
+      roomNumber:['',Validators.required],
+    })
+  }
+
+  onSubmit(){
+    if(this.reservationForm.valid){
+      console.log('valid form')
+    }
   }
 }
